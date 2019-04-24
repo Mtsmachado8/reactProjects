8c6efbf78d8cd929c4e79d007837ad747d4da405
Mtsmachado8 <mtsmachado8@gmail.com>
2019-04-24 16:57:05 -0300

    new folder location to blog-posts (beggining-stuf)

--- react-code-grider/beginning-stuf/blog-posts/src/actions/index.js
+++ react-code-grider/beginning-stuf/blog-posts/src/actions/index.js
@@ -2,8 +2,14 @@ import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // We are dispatching a function here, so redux-thunk will catch it 
+  // and automatically invoke our function transforming it to plain object 
+  // so redux can understand it
   await dispatch(fetchPosts());
 
+  console.log(`Fetchposts: ${getState().posts.map(post => post.id)}`)
+ 
+  // @TODO refactore to dont use lodash
   _.chain(getState().posts)
     .map('userId')
     .uniq()
@@ -22,6 +28,8 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+// The memoized method will record the output of that function in cache, and just get the output the second time its called
 
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 // const _fetchUser = _.memoize(async (id, dispatch) => {
